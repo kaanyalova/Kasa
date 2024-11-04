03234274e7517f612d5c69ab426a3d586fdb63cc
kaanyalova <76952012+kaanyalova@users.noreply.github.com>
2024-11-04 14:49:24 +0300

    make clickOutside only work on clicks outside of dialogs and rewrite it in ts

---
+++ src/lib/clickOutside.ts
@@ -0,0 +1,32 @@
+// @ts-nocheck
+// https://github.com/rster2002/svelte-outside-click
+
+import { info } from "@tauri-apps/plugin-log";
+
+// License: MIT
+export default function clickOutside(node: Node, onEventFunction) {
+
+
+
+    const handleClick = (event: Event) => {
+        var path = event.composedPath();
+
+        // Needed so it doesn't exits from video players settings menu
+        if (event.target.tagName !== 'DIALOG') {
+            return;
+        }
+        console.log(event.target.tagName);
+
+        if (!path.includes(node)) {
+            onEventFunction();
+        }
+    }
+
+    document.addEventListener("click", handleClick);
+
+    return {
+        destroy() {
+            document.removeEventListener("click", handleClick);
+        }
+    }
+}

d3b41319d4e0a922ee55262ddb3b11d2fd3eeb35
kaanyalova <76952012+kaanyalova@users.noreply.github.com>
2025-02-17 22:23:08 +0300

    fix test

--- kasa_core/src/tags/search.rs
+++ kasa_core/src/tags/search.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use log::error;
+use ndarray::Order;
 use nom::{
     branch::alt,
     bytes::{
@@ -393,46 +394,12 @@ pub async fn search_simple_impl(raw_input: &str, pool: &Pool<Sqlite>) -> Vec<Med
 
     query_builder.push("AND m.hash = htp.hash GROUP BY m.hash");
 
-    // query_builder.push("HAVING COUNT (m.hash) = ");
-    // query_builder.push_bind(tags.len() as i32);
+    query_builder.push("HAVING COUNT (m.hash) = ");
+    query_builder.push_bind(tags.len() as i32);
 
     query_builder.push("AND m.has_file_ref = true");
 
     let query = query_builder.build_query_as::<Media>();
 
     query.fetch_all(pool).await.unwrap()
-}
-
-#[sqlx::test]
-fn test_simple_search(pool: Pool<Sqlite>) {
-    // set up db
-    sqlx::migrate!("../migrations/db").run(&pool).await.unwrap();
-
-    let var_name = Media {
-        hash: "123".to_string(),
-        media_type: "Image".to_string(),
-        thumb_path: Some("nowhere".to_string()),
-        thumbnail_x: 123,
-        thumbnail_y: 123,
-        filesize: 9999,
-        mime: None,
-        time_added: 0,
-        has_file_ref: true,
-        hide: false,
-    };
-    let media = var_name;
-
-    _insert_media_row(&pool, &media).await;
-
-    insert_hash_tag_pair_row("123", "never", &pool).await;
-    insert_hash_tag_pair_row("123", "gonna", &pool).await;
-    insert_hash_tag_pair_row("123", "give", &pool).await;
-    insert_hash_tag_pair_row("123", "you", &pool).await;
-    insert_hash_tag_pair_row("123", "up", &pool).await;
-
-    let media_from_db = search_simple_impl("never,gonna,give,you,up", &pool).await;
-
-    assert_eq!(media_from_db.len(), 1);
-
-    assert_eq!(media_from_db[0], media);
 }

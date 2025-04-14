cf58bf05656c28df228aa45ca8c52984cb0c313e
kaanyalova <76952012+kaanyalova@users.noreply.github.com>
2025-04-15 02:21:43 +0300

    fix buggy click outside model behavior i should rewrite this whole file sometime soon

--- src/lib/clickOutside.ts
+++ src/lib/clickOutside.ts
@@ -1,6 +1,7 @@
 // https://github.com/rster2002/svelte-outside-click
 
 import { info, trace } from '@tauri-apps/plugin-log';
+import type { ActionReturn } from 'svelte/action';
 
 // License: MIT
 export function clickOutsideTagName(node: Node, onEventFunction: any, tagName: string) {
@@ -29,7 +30,6 @@ export function clickOutsideTagName(node: Node, onEventFunction: any, tagName: s
 	};
 }
 
-
 export function clickOutsideExcludingTagName(node: Node, onEventFunction: any, tagName: string) {
 	const handleClick = (event: Event) => {
 		var path = event.composedPath();
@@ -38,7 +38,7 @@ export function clickOutsideExcludingTagName(node: Node, onEventFunction: any, t
 		if ((event.target as HTMLElement).tagName === tagName) {
 			return;
 		}
-		trace(`Clicked outside of element with id ${(event.target as HTMLElement).tagName}`)
+		trace(`Clicked outside of element with id ${(event.target as HTMLElement).tagName}`);
 
 		console.log((event.target as HTMLElement).tagName);
 
@@ -52,18 +52,16 @@ export function clickOutsideExcludingTagName(node: Node, onEventFunction: any, t
 	return {
 		destroy() {
 			document.removeEventListener('click', handleClick);
-			console.log("Destory called... Somewhere")
+			console.log('Destory called... Somewhere');
 		}
 	};
 }
-
 
 export function clickOutsideClass(node: Node, onEventFunction: any, _class: string) {
 	const handleClick = (event: Event) => {
 		var path = event.composedPath();
 
 		// Needed so it doesn't exits from video players settings menu
-
 
 		//(event.target as HTMLElement).classList.forEach((v) => console.log(`Class: ${v}`))
 		if (!(event.target as HTMLElement).classList.contains(_class)) {
@@ -82,22 +80,16 @@ export function clickOutsideClass(node: Node, onEventFunction: any, _class: stri
 
 	return {
 		destroy() {
-			console.log("Destory called... Somewhere")
+			console.log('Destory called... Somewhere');
 
 			document.removeEventListener('click', handleClick);
 		}
 	};
 }
 
-
-
-
-
-
-export function clickOutsideModal(node: Node, onEventFunction: any) {
-	clickOutsideTagName(node, onEventFunction, "DIALOG");
-}
-
+//export function clickOutsideModal(node: Node, onEventFunction: any) {
+//	const destory = clickOutsideTagName(node, onEventFunction, 'DIALOG');
+//}
 
 export function clickOutside(node: Node, onEventFunction: any) {
 	const handleClick = (event: Event) => {
@@ -110,9 +102,27 @@ export function clickOutside(node: Node, onEventFunction: any) {
 
 	document.addEventListener('click', handleClick);
 
+	return () => {
+		trace('destory click outside listener');
+		document.removeEventListener('click', handleClick);
+	};
+}
+
+export function clickOutsideModal(node: Node, callback: () => void): ActionReturn {
+	const handleClick = (event: MouseEvent) => {
+		const path = event.composedPath();
+
+		if (node && !path.includes(node)) {
+			callback();
+		}
+	};
+
+	document.addEventListener('click', handleClick, true);
+
 	return {
 		destroy() {
-			document.removeEventListener('click', handleClick);
+			trace('Removing click outside listener');
+			document.removeEventListener('click', handleClick, true);
 		}
 	};
 }

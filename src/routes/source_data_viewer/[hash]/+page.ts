a3b9b289ff2f72d4c3e20ced2743c7c8f2d4aab2
kaanyalova <76952012+kaanyalova@users.noreply.github.com>
2026-02-24 19:28:29 +0300

    forgot to commit this

---
+++ src/routes/source_data_viewer/[hash]/+page.ts
@@ -0,0 +1,15 @@
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+import { commands } from '$lib/tauri_bindings';
+
+export const load: PageLoad = async ({ params }) => {
+	const sources = await commands.getMediaSources(params.hash);
+
+	return {
+		sources: sources
+	};
+
+	if (sources.length === 0) {
+		error(404, 'Not found');
+	}
+};
